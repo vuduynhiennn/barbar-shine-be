3fc275556899c1a4e001a1ec8415b0d8370e1eb1
nhienduyvu <nhienduyvu@gmail.com>
2024-02-04 07:59:32 +0700

    chore: config env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({
+  isGlobal: true,
+});
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
